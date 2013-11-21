7dd79a85570a48b1eff5c9c1c7ad5f38ce78147b
Mohammad Khan <mohkhan@zynga.com>
2013-11-21 15:31:17 -0500

    Create base WATCardOffice + WATCard interface

--- watcard.cc
+++ watcard.cc
@@ -1,1 +1,16 @@
 #include "watcard.h"
+
+
+WATCard::WATCard() : balance(0) {}
+
+void WATCard::deposit( unsigned int amount ) {
+  balance += amount;
+}
+
+void WATCard::withdraw( unsigned int amount ) {
+  balance -= amount;
+}
+
+unsigned int WATCard::getBalance() {
+  return balance;
+}
--- watcardoffice.h
+++ watcardoffice.h
@@ -2,26 +2,51 @@
 #define __WATCARDOFFICE_H__
 
 #include <uC++.h>
+#include <queue>
 #include "watcard.h"
 
 _Monitor Printer;
 
 _Task WATCardOffice {
+    struct Args {
+        unsigned int sid;
+        unsigned int amount;
+        WATCard &watcard;
+        Args( unsigned int sid, unsigned int amount, WATCard watcard) : sid(sid), amount(amount), watcard(watcard) {}
+    };
     struct Job {                           // marshalled arguments and return future
         Args args;                         // call arguments (YOU DEFINE "Args")
         FWATCard result;                   // return future
         Job( Args args ) : args( args ) {}
     };
     _Task Courier {
-
-    };                 // communicates with bank
+        void main();
+      public:
+        enum States = { Starting = "S", FundTransfer = "t", FundComplete = "T", Finished = "F" };
+        Courier( Printer &prt, Bank &bank, WATCardOffice &watCardOffice );
+        ~Courier();
+      private:
+        Printer &printer;
+        Bank &bank;
+        WATCardOffice &watCardOffice;
+    };
     void main();
   public:
     _Event Lost {};                        // uC++ exception type, like "struct"
     WATCardOffice( Printer &prt, Bank &bank, unsigned int numCouriers );
+    ~WATCardOffice();
+    enum States = { Starting = "S", Waiting = "W", Creation = "C", Transfer = "T", Finished = "F"}
     FWATCard create( unsigned int sid, unsigned int amount );
     FWATCard transfer( unsigned int sid, unsigned int amount, WATCard *card );
     Job *requestWork();
+  private:
+    uCondition waiting;
+    Printer &printer;
+    Bank &bank;
+    unsigned int numCouriers;
+    bool terminated;
+    Courier **couriers;
+    std::queue<Job*> jobs;
 };
 
 #endif
