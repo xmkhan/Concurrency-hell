7e0caed2c4ff916cbcc3aab6615860030b8a3915
Alireza <alireza.pourhaj@gmail.com>
2013-11-21 00:41:33 -0500

    q2printer deleted

--- student.cc
+++ student.cc
@@ -1,1 +1,14 @@
 #include "student.h"
+
+void Student::main() {	
+	wc = wco->create(id, 5);
+	vm = ns->getMachine(id);
+
+	while ( numPurchases > 0 ) {
+		yiled(RNG(1, 10));
+	}
+}
+
+Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
+             unsigned int maxPurchases ): prt(&prt), ns(&nameServer), wco(&WATCardOffice),
+             id(id), flavour(VendingMachine::Flavours(RNG(3))), numPurchases(RNG(1, maxPurchases)) {}
--- student.h
+++ student.h
@@ -2,12 +2,22 @@
 #define __STUDENT_H__
 
 #include <uC++.h>
+#include "watcard.h"
 
 _Monitor Printer;
 _Task NameServer;
 _Task WATCardOffice;
+enum VendingMachine::Flavour;
 
 _Task Student {
+    FWATCard* wc;
+    Printer* prt;
+    NameServer* ns;
+    WATCardOffice* wco;
+    VendingMachine* vm;
+    VendingMachine::Flavour flavour;
+    unsigned int, id, numPurchases;
+
     void main();
   public:
     Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
