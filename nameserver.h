4efea22bf11157f6f49cd5460ca27401fdb6fa38
Alireza <alireza.pourhaj@gmail.com>
2013-11-23 03:35:31 -0500

    initial commit for name server, truck, and vending machine

--- nameserver.h
+++ nameserver.h
@@ -5,9 +5,21 @@
 #include "vendingmachine.h"
 
 _Monitor Printer;
+_Task VendingMachine;
 
 _Task NameServer {
+	Printer& prt;
+	unsigned int numVendingMachines, numStudents;
+	VendingMachine **vms;
+	int *studentToVM;
+	unsigned int sIndex, vmIndex;
+	uCondition waiting;
+
+	enum States = { Starting = "S", RegisterVendingMachine = "R", 
+	                NewVendingMachine = "N", Finished = "F" };
+
     void main();
+    
   public:
     NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents );
     void VMregister( VendingMachine *vendingmachine );
--- truck.h
+++ truck.h
@@ -8,6 +8,24 @@ _Monitor NameServer;
 _Task Truck;
 
 _Task Truck {
+	Printer &prt;
+	NameServer &ns;
+	BottlingPlant &plant;
+	unsigned int numVendingMachines, maxStockPerFlavour, 
+	             addToVM, outOfFlavour, totalNotReplenished,
+	             curCargoSize;
+
+	VendingMachine **machineList;
+	unsigned int *cargo;
+	unsigned int *inventory;
+
+
+	enum States = { Starting = "S", ShipmentPickedUp = "P", 
+	                Deliver = "d", UnsuccessfulRestock = "U", 
+	                EndDelivery = "D", Finished = "F" };
+    
+    void restockVendingMachine();
+    void restockFlavour();
     void main();
   public:
     Truck( Printer &prt, NameServer &nameServer, BottlingPlant &plant,
