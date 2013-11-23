6b77f0df6a2f7dd85a9603acc6d6571b49fb381c
Mohammad Khan <mohkhan@zynga.com>
2013-11-23 03:43:59 -0500

    Added student

--- student.cc
+++ student.cc
@@ -1,1 +1,49 @@
 #include "student.h"
+#include "MPRNG.h"
+#include "printer.h"
+#include "nameserver.h"
+#include "watcardoffice.h"
+
+Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id, unsigned int maxPurchases )
+: printer(prt),
+  nameServer(nameServer),
+  watCardOffice(cardOffice),
+  id(id),
+  maxPurchases(maxPurchases) {
+
+  }
+Student::~Student() { }
+
+void Student::main() {
+  unsigned int numberOfBottles = RNG(1, maxPurchases);
+  unsigned int favouriteFlavour = RNG(0, 3);
+  WATCard::FWATCard watCard = watCardOffice.create(id, 5);
+  VendingMachine *vendingMachine = nameServer.getMachine(id);
+
+  unsigned int purchasedBottles = 0;
+  bool retry = false;
+  while (purchasedBottles < numberOfBottles) {
+    yield(RNG(1, 10));
+      try{
+        L:
+          if (retry) watCard = watCardOffice.create(id, 5);
+          VendingMachine::Status status = vendingMachine->buy(favouriteFlavour, *watCard());
+          switch (status) {
+            case FUNDS:
+              watCard = watCardOffice.transfer(vendingMachine->cost() + 5);
+              break;
+            case STOCK:
+              vendingMachine = nameServer.getMachine(id);
+              retry = false;
+              goto L;
+            case BUY:
+            default: break;
+          }
+          retry = false;
+          purchasedBottles += 1;
+      } catch (watCardOffice::Lost &e) {
+        retry = true;
+        goto L;
+      }
+  }
+}
